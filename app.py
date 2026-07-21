8c60f1effcb7f5562140c45c82245fac4bf8ca3c
Jack Peters <jp24147@students.sacredheart.school.nz>
2026-07-21 21:26:53 +0000

    finished tutorial

--- app.py
+++ app.py
@@ -12,3 +12,13 @@ def about():
     return template('about')
 
 @route('/contact')
+@view('contact')
+def contact():
+    return {}
+
+@route('/static/<filename>')
+def server_static(filename):
+    return static_file(filename, root='./static')
+
+if __name__ == "__main__":
+    run(host='localhost',port=8080,debug=True, reloader=True)
